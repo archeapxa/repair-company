20743b538d945f4acd0d19e9e8e60efcaabd4924
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-03-04 11:00:59 +0200

    feat: add normalize, grid, variables, templates, header style in template folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+// const cleanCSS = require('gulp-clean-css');
+// const rename = require('gulp-rename');
 
 // sass
 function serveSass() {
@@ -10,7 +10,7 @@ function serveSass() {
       .pipe(sass())
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
-};
+}
 
 // Static server
 function bs() {
@@ -21,9 +21,9 @@ function bs() {
       }
   });
   watch("./*.html").on('change', browserSync.reload); 
-  watch(".sass/**/*.sass", serveSass); 
+  watch("./sass/**/*.sass", browserSync.reload); 
   watch("./js/*.html").on('change', browserSync.reload);
-};
+}
 
 
 
