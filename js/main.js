5f8eb02745e7d171233fd59d34ef8a15ac7dafe2
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-03-11 21:03:07 +0200

    feat: added js to close window on esc button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   //do work
   const modal = document.querySelector('.modal');
+  const modalDialog = document.querySelector('.modal__dialog');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
@@ -8,9 +9,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+    element.addEventListener('click', () => {
+      modal.classList.add('modal--visible');
+    }); // добавляем класс
   });
 
-  closeBtn.addEventListener('click', switchModal);
+  closeBtn.addEventListener('click', switchModal); // переключаем(закрываем) модал по клику на closeBtn
+
+  document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27) {
+      modal.classList.remove('modal--visible');//удаляем класс при отпускании esc
+    }
+  });
+
+  
 
 });
+
+  // modalBtn.forEach(element => {
+  //   element.addEventListener('click', switchModal); // переключаем по клику на любой из елементов подходящих под modalBtn
+  // });
+
+
+  // modalBtn.forEach(element => {
+  //   element.addEventListener('click', switchModal); // переключаем по клику на любой из елементов подходящих под modalBtn
+  // });
+
+
+  // document.addEventListener('click', (e) => {
+  //   if (e.target.className != 'modalDialog') {
+  //     modal.classList.toggle('modal--visible');
+  //   } 
+  // })
+
+    // if(e.keyCode === 27) {
+    //   modal.classList.toggle('modal--visible');
+    //   console.log('e.keyCode');
+    // }
