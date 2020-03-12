c4cf786a4858e11dabab17764d7766b6d0da6603
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-03-12 13:12:18 +0200

    feat: added button ToTop, in html and js

--- js/main.js
+++ js/main.js
@@ -61,4 +61,20 @@ $(document).ready(function () {
     };
   });
 
+
+
+  $(function() {  //появление кнопки Вверх при прокрутке страницы, промотка страницы вверх при нажатии
+    $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+    $('#toTop').fadeIn();
+    } else {
+    $('#toTop').fadeOut();
+    }
+    });
+    $('#toTop').click(function() {
+    $('body,html').animate({scrollTop:0},800);
+    });
+    });
+
+
 });
