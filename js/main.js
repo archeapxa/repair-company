ba473cc1b2855fb9e698f85b73af6dcf98756f71
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-03-13 19:13:34 +0200

    feat: moblie version for projects section

--- js/main.js
+++ js/main.js
@@ -89,13 +89,15 @@ $(document).ready(function () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    slidesPerView: 1,
   })
 
   var next = $('.swiper-button-next');
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
 
-  next.css('left', prev.width() + 10 + bullets.width() + 10)
-  bullets.css('left', prev.width() +10)
+  console.log(prev)
+  next.css('left', prev.width() + 10 + bullets.width() + 10 + prev.position().left)
+  bullets.css('left', prev.width() + 10 + prev.position().left)
 
 });
