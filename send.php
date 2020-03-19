85ef7e48c7ec8b477a1bdbafe08317d6f635a336
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-03-19 14:55:04 +0200

    fix: smtp debug off

--- send.php
+++ send.php
@@ -26,7 +26,7 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
     //Server settings
-    $mail->SMTPDebug = 2;                      // Enable verbose debug output
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
