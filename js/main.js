3c99ada580087a34594045512d9b34ee5661dff8
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-03-24 22:47:31 +0200

    cursor poiner, link scroll effect

--- js/main.js
+++ js/main.js
@@ -518,5 +518,15 @@ $(document).ready(function () {
 
   });
 
+// scroll
+
+  $("body").on('click', '[href*="#"]', function(e){
+    var fixed_offset = 150;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
+
+
+
 
 });
